4d23e98dc923eb88d33cfbf32d7d19b8beec0882
Emmanuel Stamatogiannakis <estamatogiannakis@bayareametro.gov>
2019-06-10 16:21:10 -0700

    express lane name changes

--- scripts/net_spec_futures_round1.py
+++ scripts/net_spec_futures_round1.py
@@ -77,8 +77,9 @@ COMMITTED_PROJECTS = collections.OrderedDict([
     (2025, {
         'hwy':[{'name':'Bridge_Toll_Updates', 'kwargs':{'MODELYEAR':'2025'}},
                'EXP_CC_050028_I680_SB_HOV_Completion',
-               'EXP_101B',
-               'EXP_680C',
+               'EXP_101B1',
+               'EXP_101B2'
+               'EXP_680C1',
                'EXP_85C',
                'EXP_101C',
                'ALA150001_I680_SR84_Int_Wid',
@@ -97,7 +98,7 @@ COMMITTED_PROJECTS = collections.OrderedDict([
     }),
     (2030, {
         'hwy':[{'name':'Bridge_Toll_Updates', 'kwargs':{'MODELYEAR':'2030'}},
-               'EXP_880B'],
+               'EXP_880A'],
         'trn':['BART_NoProject']
     }),
     (2035, {
