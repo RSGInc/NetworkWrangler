8d7e2622c930c9e23ead2fed701bbe0f6705debd
bespinmtc <bespin@mtc.ca.gov>
2020-09-25 16:20:44 -0700

    TSP on SR-29 project

--- scripts/net_spec_blueprint.py
+++ scripts/net_spec_blueprint.py
@@ -167,6 +167,7 @@ BLUEPRINT_PROJECTS = collections.OrderedDict([
                        'FBP_MU_029_ACRapid_2025',
                        'FBP_MR_026_NovatoWide',
                        'FBP_CC_054_CrowCanyonWide',
+                       'TSP on SR-29',
                        'FBP_CC_050_SR4_Operation_Improvements_EB'],
                 'trn':['RRSP_E14_Mission_Corridor',
                        'Transform_SR37_Widening_Interim',
