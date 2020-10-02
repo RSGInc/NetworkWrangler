eae8e7fd6659831c9fce3638c2873754f9d81e82
bespinmtc <bespin@mtc.ca.gov>
2020-10-02 13:28:59 -0700

    Removed duplicate project

--- scripts/net_spec_blueprint.py
+++ scripts/net_spec_blueprint.py
@@ -219,13 +219,11 @@ BLUEPRINT_PROJECTS = collections.OrderedDict([
                        'RRSP_East_West_Connector',
                        'EXP_Blueprint',
                        'Transform_I680_Multimodal_Imp',
-                       'MAJ_MuniForward_Uncommitted',
                        'FBP_SM_022_I380_Widening'],
                 'trn':['MAJ_MuniForward_Uncommitted',
                        'RRSP_South_East_Waterfront_Transit_Imp',
                        'FBP_MU_062_ReX_Red',
                        'Transform_I680_Multimodal_Imp',
-                       'MAJ_MuniForward_Uncommitted',
                        'MAJ_Treasure_Island_Congestion_Pricing']
         }),
         (2040, {'hwy':['BP_Vision_Zero',
