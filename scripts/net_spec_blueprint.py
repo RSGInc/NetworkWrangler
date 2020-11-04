eb9c6ce2ce7263c2201ef3c5f4e6bb9429a8b337
bespinmtc <bespin@mtc.ca.gov>
2020-11-03 16:20:09 -0800

    Added transit component of Fremont Decoto Road Complete Streets (Union City-Fremont East-West Connector)

--- scripts/net_spec_blueprint.py
+++ scripts/net_spec_blueprint.py
@@ -247,6 +247,7 @@ BLUEPRINT_PROJECTS = collections.OrderedDict([
                        'Transform_I680_Multimodal_Imp',
                        'Transform_SeamlessTransit',
                        'MAJ_Treasure_Island_Congestion_Pricing',
+                       'RRSP_East_West_Connector',
                        {'name':'MAJ_SOL070020_I80_I680_SR12_Int_2B_7','kwargs':{'PHASE':"'3'"}},
                        {'name':'MAJ_SOL070020_I80_I680_SR12_Int_2B_7','kwargs':{'PHASE':"'4'"}},
                        {'name':'MAJ_SOL070020_I80_I680_SR12_Int_2B_7','kwargs':{'PHASE':"'5'"}}]
