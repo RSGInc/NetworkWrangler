0334f82fc8900c6a0b17875bb35cd6e7b6a33462
Lisa Zorn <lzorn@bayareametro.gov>
2023-08-04 19:11:59 -0700

    Handle ACE frequency updates using GTFS

--- scripts/net_spec_blueprint.py
+++ scripts/net_spec_blueprint.py
@@ -122,6 +122,9 @@ COMMITTED_PROJECTS = collections.OrderedDict([
                {'name':'Transit_Frequency_Updates_Using_511', 
                 'kwargs':{'regional_gtfs_zip':'r"M:\\Data\\Transit\\511\\2023-05.zip"', 
                           'operator':'"Bay Area Rapid Transit"'}},
+               {'name':'Transit_Frequency_Updates_Using_511', 
+                'kwargs':{'regional_gtfs_zip':'r"M:\\Data\\Transit\\511\\2023-05.zip"', 
+                          'operator':'"Altamont Corridor Express"'}},
                ]
     }),
     (2025, {
