7aa969007e34b86139fb75377096b107be20a185
Lufeng Lin <llin@bayareametro.gov>
2023-08-11 11:56:51 -0700

    Add VTA to Transit_Frequency_Updates_Using_511 in order to update freq by gtfs

--- scripts/net_spec_blueprint.py
+++ scripts/net_spec_blueprint.py
@@ -131,6 +131,9 @@ COMMITTED_PROJECTS = collections.OrderedDict([
                {'name':'Transit_Frequency_Updates_Using_511', 
                 'kwargs':{'regional_gtfs_zip':'r"M:\\Data\\Transit\\511\\2023-05.zip"', 
                           'operator':'"San Francisco Municipal Transportation Agency"'}},                          
+               {'name':'Transit_Frequency_Updates_Using_511', 
+                'kwargs':{'regional_gtfs_zip':'r"M:\\Data\\Transit\\511\\2023-05.zip"', 
+                          'operator':'"VTA"'}},                                                    
                ]
     }),
     (2025, {
