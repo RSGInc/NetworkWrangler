4c0d550847f22dfd5b324aeb3ac33bc3c585400d
Lufeng Lin <llin@bayareametro.gov>
2023-09-11 10:34:32 -0700

    add Sonoma County Transit to Transit_Frequency_Updates_Using_511

--- scripts/net_spec_blueprint.py
+++ scripts/net_spec_blueprint.py
@@ -184,6 +184,9 @@ COMMITTED_PROJECTS = collections.OrderedDict([
                 {'name':'Transit_Frequency_Updates_Using_511', 
                 'kwargs':{'regional_gtfs_zip':'r"M:\\Data\\Transit\\511\\2023-05.zip"', 
                           'operator':'"VINE Transit"'}}, 
+                {'name':'Transit_Frequency_Updates_Using_511', 
+                'kwargs':{'regional_gtfs_zip':'r"M:\\Data\\Transit\\511\\2023-05.zip"', 
+                          'operator':'"Sonoma County Transit"'}},
         ]
     }),
     (2025, {
